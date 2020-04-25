d51c87aa1552cba0dc3ed288429e3f9308596549
LukasRimk <lukas.rmk@gmail.com>
2020-04-25 14:34:17 +0300

    TripsController, Trips CRUD.

---
+++ Vezu-Vaziuoju/Controllers/TripsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vezu_Vaziuoju;
+using Vezu_Vaziuoju.Models;
+
+namespace Vezu_Vaziuoju.Controllers
+{
+    public class TripsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Trips
+        public ActionResult Index()
+        {
+            var trips = db.Trips.Include(t => t.Post).Include(t => t.TripState);
+            return View(trips.ToList());
+        }
+
+        // GET: Trips/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // GET: Trips/Create
+        public ActionResult Create()
+        {
+            ViewBag.PostId = new SelectList(db.Posts, "Id", "AddressFrom");
+            ViewBag.State = new SelectList(db.TripStates, "Id", "Name");
+            return View();
+        }
+
+        // POST: Trips/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,StartTime,EndTime,EndedByDriver,State,PostId")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trips.Add(trip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PostId = new SelectList(db.Posts, "Id", "AddressFrom", trip.PostId);
+            ViewBag.State = new SelectList(db.TripStates, "Id", "Name", trip.State);
+            return View(trip);
+        }
+
+        // GET: Trips/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PostId = new SelectList(db.Posts, "Id", "AddressFrom", trip.PostId);
+            ViewBag.State = new SelectList(db.TripStates, "Id", "Name", trip.State);
+            return View(trip);
+        }
+
+        // POST: Trips/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,StartTime,EndTime,EndedByDriver,State,PostId")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trip).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PostId = new SelectList(db.Posts, "Id", "AddressFrom", trip.PostId);
+            ViewBag.State = new SelectList(db.TripStates, "Id", "Name", trip.State);
+            return View(trip);
+        }
+
+        // GET: Trips/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trip trip = db.Trips.Find(id);
+            db.Trips.Remove(trip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
